75fe19f993c426e4c2069e7c08f8165ea4c5fdf3
Nazar <nrashchynski@gmail.com>
2025-04-21 09:07:53 +0300

    Update main.cpp

--- Lab3/src/main.cpp
+++ Lab3/src/main.cpp
@@ -80,10 +80,10 @@ int main() {
 		h_arr[i] = x[i] - x[i - 1];
 	}
 
-	C[0] = 1;
-	C[n] = h_arr[n] / 3;
+	C[0] = -1;   // Добавляем - перед C 
+	C[n] = -h_arr[n] / 3;
 	for (int i = 1; i < n; i++) {
-		C[i] = (h_arr[i] + h_arr[i + 1]) / 3;
+		C[i] = -(h_arr[i] + h_arr[i + 1]) / 3;
 	}
 
 	A[n] = h_arr[n] / 6;
@@ -93,10 +93,10 @@ int main() {
 		B[i] = h_arr[i + 1] / 6;
 	}
 
-	F[0] = d2f(x[0]);  // S''(x0) = f''(x0)
-	F[n] = df(x[n]) - (y[n] - y[n-1]) / h_arr[n];  // S'(xn) = f'(xn)
+	F[0] = -d2f(x[0]);  // S''(x0) = f''(x0)    // И перед F
+	F[n] = -(df(x[n]) - (y[n] - y[n-1]) / h_arr[n]);  // S'(xn) = f'(xn)
 	for (int i = 1; i < n; i++) {
-		F[i] = (y[i + 1] - y[i]) / h_arr[i + 1] - (y[i] - y[i - 1]) / h_arr[i];
+		F[i] = -((y[i + 1] - y[i]) / h_arr[i + 1] - (y[i] - y[i - 1]) / h_arr[i]);
 	}
 
 	M = runThrowMethod(A, B, C, F);
